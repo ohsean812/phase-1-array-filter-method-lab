4a2d3135116328df7353d1f61ff917498128f0b2
Sean <oseanwave@gmail.com>
2023-04-20 17:43:52 -0400

    commit tree 1b18a10f04fe988d229afd2c2f8f7eee94cd5162

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, letters) {
+    return array.filter(driver => driver.toLowerCase().indexOf(letters.toLowerCase()) === 0)
+}
+
+function matchName(array, string) {
+    return array.filter(driver => driver.name === string)
+}
